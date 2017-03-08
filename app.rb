c600c467c22a194d80f595ea0eb504a2c9b27158
Albert <ayanit1@googlemail.com>
2017-03-08 22:28:05 +0000

    add new link feature with specs

--- app.rb
+++ app.rb
@@ -11,4 +11,13 @@ class App < Sinatra::Base
     @links = Link.all
     erb :index
   end
+
+  get '/links/new' do
+    erb :'links/new'
+  end
+
+  post '/links' do
+    Link.create(url: params[:url], title: params[:title])
+    redirect '/links'
+  end
 end
--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -1,8 +1,12 @@
+# As a time-pressed user
+# So that I can save a website
+# I would like to add the site's address and title to my bookmark manager
+
 feature 'Creating links' do
   scenario 'I can create a new link' do
-    visit '/link/new'
-    fill_in 'url', with: 'http://www.zombo.com/'
-    fill_in 'title', with: 'This is Zombocom'
+    visit '/links/new'
+    fill_in :url , with: 'http://www.zombo.com/'
+    fill_in :title , with: 'This is Zombocom'
     click_button 'Create link'
     expect(current_path).to eq '/links'
     within 'ul#links' do
